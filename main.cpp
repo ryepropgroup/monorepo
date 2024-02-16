7b4cb4a651e998e5e6b9c0077683b0e8c2b9bc4b
zazu7765 <ohmyzazu@gmail.com>
2024-02-16 18:48:51 -0500

    feat(git submodules)
    Signed-off-by: zazu7765 <ohmyzazu@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 #include <boost/asio.hpp>
 #include <spdlog/spdlog.h>
+#include <yaml-cpp/yaml.h>
 //
 // Created by danie on 2023-12-14.
 //
 
 int main() {
     spdlog::info("Hello, {}!", "World");
+    YAML::Emitter out;
+    out << YAML::BeginSeq;
+    out << "eggs";
+    out << "bread";
+    out << "milk";
+    out << YAML::EndSeq;
+    std::cout<< out.c_str()<<std::endl;
 }
