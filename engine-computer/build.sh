8b83fd6902215d6155e947f1363240de90006933
zazu7765 <41345110+zazu7765@users.noreply.github.com>
2024-06-02 13:05:53 -0400

    Update Scripts and CMake for MacOS

--- engine-computer/build.sh
+++ engine-computer/build.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 echo "********** MACH: Configuring the build with CMake **********"
 cmake --preset=linux
 
+else
+export CC=/opt/homebrew/bin/gcc-13
+export CXX=/opt/homebrew/bin/g++-13
+cmake --preset=macos
+fi
+
 echo "********** MACH: Building the project **********"
-cmake --build build
+cmake --build build 
+cp ./build/engine_computer ../build/bin
