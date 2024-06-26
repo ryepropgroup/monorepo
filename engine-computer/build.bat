347d9e7d543840435c19c5f805299fc9483f0e21
biscuut <20166775+biscuut@users.noreply.github.com>
2024-06-25 23:21:21 -0400

    Move vcpkg to root

--- engine-computer/build.bat
+++ engine-computer/build.bat
@@ -6,14 +6,14 @@ for /f "usebackq tokens=*" %%i in (`vendor\vswhere\vswhere -latest -products * -
 
 if not defined DevEnvDir (
     if exist "%InstallDir%\Common7\Tools\vsdevcmd.bat" (
-        echo ********** MACH: Using Visual Studio at **********
+        @REM echo ********** MACH: Using Visual Studio at %InstallDir% **********
         "%InstallDir%\Common7\Tools\vsdevcmd.bat" %* & "%~nx0" %*
     ) else (
         echo ********** MACH: Visual Studio not found, exiting! **********
         exit /b 1
     )
 ) else (
-    echo ********** MACH: Already using Visual Studio at **********
+    @REM echo ********** MACH: Already using Visual Studio at %InstallDir% **********
 )
 
 echo ********** MACH: Configuring the build with CMake **********
