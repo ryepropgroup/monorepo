6feda1eec00d2e941385785e416f35d2ed1489da
zazu7765 <41345110+zazu7765@users.noreply.github.com>
2024-06-30 00:57:12 -0400

    Update build script, move to root folder

---
+++ build.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+export VCPKG_BUILD_TYPE=release
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    echo "********** MACH: Configuring the build with CMake **********"
+    cmake engine-computer --preset=linux
+else
+    export CC=/opt/homebrew/bin/gcc-14
+    export CXX=/opt/homebrew/bin/g++-14
+    echo "********** MACH: Configuring the build with CMake **********"
+    cmake engine-computer --preset=macos
+fi
+
+TOOLS_DIR="./tools"
+
+if [ ! -d "$TOOLS_DIR" ]; then
+    mkdir -p "$TOOLS_DIR"
+    echo "Directory created: $TOOLS_DIR"
+else
+    echo "Directory already exists: $TOOLS_DIR"
+    rm -rf "$TOOLS_DIR"
+    mkdir -p "$TOOLS_DIR"
+fi
+
+case $(uname -s) in
+    Linux*)     OSPATH=x64-linux;;
+    Darwin*)    OSPATH=arm64-osx;;
+    *)          OSPATH=windows;;
+esac
+
+mv ./engine-computer/build/vcpkg_installed/$OSPATH/tools "./"
+if [ $? -eq 0 ]; then
+    echo "Tools moved to $TOOLS_DIR"
+else
+    echo "Failed to move tools to $TOOLS_DIR"
+fi
+
+cd ./engine-computer
+echo "********** MACH: Building the project **********"
+cmake --build build # -DCMAKE_BUILD_TYPE=MinSizeRel
+cp ./build/engine_computer ../build/bin
