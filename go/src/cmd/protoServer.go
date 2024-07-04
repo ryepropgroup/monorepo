8bb75b807a08cbe87c48ab0eb122c421f63ef93c
zazu7765 <41345110+zazu7765@users.noreply.github.com>
2024-07-03 21:15:22 -0400

    Create basic TCP/gRPC go implementations

--- go/src/cmd/protoServer.go
+++ go/src/cmd/protoServer.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"github.com/ryepropgroup/protoServer/protos/messages"
+	"github.com/ryepropgroup/protoServer/rpcService"
+	"github.com/ryepropgroup/protoServer/tcp"
+)
 
 func main() {
-	fmt.Println("Hello MACH!")
+	commandChan := make(chan *messages.SequenceCommand)
+	statusChan := make(chan *messages.SensorData)
+
+	go tcp.StartTCPServer(commandChan, statusChan)
+
+	rpcService.StartGRPCServer(commandChan, statusChan)
+
 }
