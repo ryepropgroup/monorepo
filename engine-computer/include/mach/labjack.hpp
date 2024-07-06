bb67f63c7ab94bd372a1aff6efe59a2b447cadbd
biscuut <20166775+biscuut@users.noreply.github.com>
2024-07-06 02:13:21 -0400

    Change config to allow for multiple labjacks with their own valves and sensors - Updated labjack settings to be more simple - Each device can now have a multiplier which will be applied to the analog value. Thermocouples now use the CJC automatically

--- engine-computer/include/mach/labjack.hpp
+++ engine-computer/include/mach/labjack.hpp
@@ -2,9 +2,10 @@
 #include <string>
 #include <vector>
 #include <memory>
-#include "device/device.hpp"
 
 namespace mach {
+
+class Sensor;
 
 class LabJack {
     public:
@@ -14,11 +15,20 @@ class LabJack {
         LabJack& operator=(const LabJack&) = delete;
 
         int getHandle();
+        void startStreaming();
+        void readStream();
+        void addSensor(std::shared_ptr<Sensor> sensor);
         void setHigh(std::string port);
         void setLow(std::string port);
+        std::string getName() { return labjackName; }
+        void setCJCValue(double value);
+        double getCJCValue() { return cjcValue; }
 
     private:
+        std::string labjackName;
         int handle;
+        std::vector<std::shared_ptr<Sensor>> sensors;
+        double cjcValue = 0;
 };
 
 } // namespace mach
